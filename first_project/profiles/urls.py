9dde6f599370dd550da8516683aae5d5246dfc0b
Musa Naeem <musa.naeem@arbisoft.com>
2022-08-22 14:32:52 +0500

    Separated css and removed JS

--- first_project/profiles/urls.py
+++ first_project/profiles/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profiles/', views.login_page),
+    path('profiles/', views.login_user),
     path('profiles/index', views.index, name="home"),
-    path('profiles/register', views.register_page, name="register"),
-    path('profiles/login', views.login_page, name="login")
+    path('profiles/register', views.register_user, name="register"),
+    path('profiles/login', views.login_user, name="login")
 
 ]
